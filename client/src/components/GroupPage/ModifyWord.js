be8a9ea177eff89f2ae86a363b6e96d46083b4da
Tapio Salonen <tsa.dom@pm.me>
2021-12-03 19:46:02 +0200

    Small changes

---
+++ client/src/components/GroupPage/ModifyWord.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ModifyWord = ({ word }) => {
+  console.log(word)
+  return (
+    <>test</>
+  )
+}
+
+export default ModifyWord
--- client/src/components/GroupPage/index.js
+++ client/src/components/GroupPage/index.js
@@ -11,6 +11,7 @@ import AddWord from './AddWord'
 import { useTranslation } from 'react-i18next'
 import useGroups from '../../hooks/groups'
 import { modifyGroup as modify } from '../../features/groupSlice'
+import ModifyWord from './ModifyWord'
 
 const GroupName = ({ group }) => {
   const [editMode, setEditMode] = useState(false)
@@ -59,8 +60,11 @@ const GroupName = ({ group }) => {
 const GroupPage = () => {
   const group = useSelector(state => state.groups.selectedGroup)
   const { t } = useTranslation('translation')
-  const [selected, setSelected] = useState('group-description')
+  const [selectedPage, setSelectedPage] = useState('group-description')
+  const [selectedWord, setSelectedWord] = useState(null)
   const history = useHistory()
+
+  const items = ['group-description', 'group-word-list', 'group-word-add']
 
   useEffect(() => {
     if (!group) history.push('/groups')
@@ -79,15 +83,24 @@ const GroupPage = () => {
         </div>
         <hr className="page-container-linebreak"></hr>
         <MenuBar
-          selected={selected}
-          setSelected={setSelected}
-          items={['group-description', 'group-word-list', 'group-word-add']}
+          selected={selectedPage}
+          setSelected={setSelectedPage}
+          items={items.includes(selectedPage) ? items : items.concat(selectedPage)}
         />
       </div>
       <div className="page-container-body">
-        {selected === 'group-description' && <Description group={group} />}
-        {selected === 'group-word-list' && <List group={group} />}
-        {selected === 'group-word-add' && <AddWord setSelected={setSelected} group={group} />}
+        {selectedPage === 'group-description' && <Description group={group} />}
+        {selectedPage === 'group-word-list' &&
+          <List
+            group={group}
+            setSelectedWord={setSelectedWord}
+            setSelectedPage={setSelectedPage}
+          />
+        }
+        {selectedPage === 'group-word-add' && <AddWord setSelected={setSelectedPage} group={group} />}
+        {!items.includes(selectedPage) &&
+          <ModifyWord word={selectedWord} />
+        }
       </div>
     </>
   )
